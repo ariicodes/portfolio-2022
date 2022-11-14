5e90bccfb91ea4b29ac695119715aed7ca38c16a
ariicodes <arielaisrael12@gmail.com>
2022-11-13 23:22:20 -0500

    font changed & letter spacing added

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,11 +1,13 @@
 const Title = () => {
-    return (
-			<header className='flex flex-col items-end pt-5 font-bold'>
-				<h1 className='font-PFSC text-3xl mr-6 lg:text-4xl'>Ariela Israel</h1>
-				<p className='mt-1 mb-4 mr-6 font-PF italic text-pale-cerulean text-xs lg:text-sm'>designer & developer</p>
-				<hr className='w-3/4 h-2 mr-0 md:w-5/12 lg:w-5/12 border-white/50' />
-			</header>
-		)
+	return (
+		<header className='flex flex-col items-end pt-5 font-bold tracking-wider'>
+			<h1 className='font-Urbanist text-3xl mr-6 lg:text-4xl'>Ariela Israel</h1>
+			<p className='mt-1 mb-4 mr-6 font-Urbanist italic text-pale-cerulean text-xs lg:text-sm'>
+				designer & developer
+			</p>
+			<hr className='w-3/4 h-2 mr-0 md:w-5/12 lg:w-5/12 border-white/50' />
+		</header>
+	)
 }
 
 export default Title
