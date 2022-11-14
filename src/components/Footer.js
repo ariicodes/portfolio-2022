65a6eb00d70b1e7b83e097274f3b37b686ac8aaf
ariicodes <arielaisrael12@gmail.com>
2022-11-13 23:06:17 -0500

    font changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 const Footer = () => {
 	return (
-		<div className='italic text-xs font-PF pr-6 pb-5 pt-2 flex flex-row justify-end gap-5'>
+		<div className='italic text-xs font-Urbanist pr-6 pb-5 pt-2 flex flex-row justify-end gap-5'>
 			<a
 				className='text-blush hover:text-pale-cerulean'
 				href='https://github.com/ariicodes/portfolio-2022'
