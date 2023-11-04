98cb8642e770052eed06ac7835b0e267177fb010
Ariela Israel <arielaisrael12@gmail.com>
2023-11-03 22:51:54 -0400

    project cards resigned and project updated

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,115 +1,57 @@
-import github from '../assets/github.svg'
-import link from '../assets/link.svg'
-import sun from '../assets/sun.png'
+import github from '../assets/github.svg';
+import link from '../assets/link.svg';
+// import sun from '../assets/sun.png';
 
-const Project = () => {
+const Project = ({ icon, altText, repo, url, title }) => {
 	return (
-		<div className='flex flex-col font-Urbanist w-8/12 pt-6 pl-6 absolute right-1/3 gap-4 md:-left-0.5 lg:text-xl lg:w-4/12 lg:flex-row lg:gap-6'>
-			<div className='flex flex-col gap-1.5'>
-				<div className='flex gap-4'>
-					<div className='flex flex-row justify-center items-center bg-gradient-to-b from-slate-900 to-slate-800 bg-no-repeat bg-cover rounded-md'>
-						<img
-							src={sun}
-							alt='sun icon'
-							width='100'
-							height='100'
-						/>
-					</div>
-					<div className='flex flex-col justify-around py-3 italic'>
-						<a
-							href='https://www.github.com/ariicodes/shyne-weather'
-							target='_blank'
-							rel='noreferrer'
-							className='flex gap-2'
-						>
-							<img
-								className='w-6'
-								src={github}
-								alt='github logo'
-							/>
-							<p className='text-sm underline underline-offset-4 hover:text-pale-cerulean'>repo</p>
-						</a>
-						<a
-							href='https://shyne-weather.web.app'
-							target='_blank'
-							rel='noreferrer'
-							className='flex gap-2'
-						>
-							<img
-								className='w-6'
-								src={link}
-								alt='link icon'
-							/>
-							<p className='text-sm underline underline-offset-4 hover:text-pale-cerulean'>live app</p>
-						</a>
-					</div>
+		<div className='flex flex-col gap-1.5'>
+			<div className='flex gap-4'>
+				<div className='flex flex-row justify-center items-center w-28 p-2 bg-gradient-to-b from-blush to-pale-cerulean bg-no-repeat bg-cover rounded-md'>
+					<img
+						src={icon}
+						alt={altText}
+						width='180'
+						height='150'
+					/>
 				</div>
-				<div>
-					<h2 className='text-xl font-semibold'>Shyne Weather</h2>
+				<div className='flex flex-col justify-around py-3 italic'>
+					<a
+						href={repo}
+						target='_blank'
+						rel='noreferrer'
+						className='flex gap-2'
+					>
+						<img
+							className='w-6'
+							src={github}
+							alt='github logo'
+						/>
+						<p className='text-sm underline underline-offset-4 hover:text-pale-cerulean'>
+							repo
+						</p>
+					</a>
+					<a
+						href={url}
+						target='_blank'
+						rel='noreferrer'
+						className='flex gap-2'
+					>
+						<img
+							className='w-6'
+							src={link}
+							alt='link icon'
+						/>
+						<p className='text-sm underline underline-offset-4 hover:text-pale-cerulean'>
+							live app
+						</p>
+					</a>
 				</div>
 			</div>
-			{/* <div className='flex flex-col gap-1.5'>
-				<div className='flex gap-4'>
-					<div className='w-72 h-32 bg-platinum rounded-md'></div>
-					<div className='flex flex-col justify-around py-3'>
-						<a
-							href='https://www.github.com/ariicodes'
-							target='_blank'
-							rel='noreferrer'
-						>
-							<img
-								className='w-6'
-								src={github}
-								alt='github logo'
-							/>
-						</a>
-						<a
-							href='/'
-							target='_blank'
-							rel='noreferrer'
-						>
-							<img
-								className='w-6'
-								src={link}
-								alt='link icon'
-							/>
-						</a>
-					</div>
-				</div>
-				<h2>project name</h2>
-			</div> */}
-			{/* <div className='flex flex-col gap-1.5'>
-				<div className='flex gap-4'>
-					<div className='w-72 h-32 bg-platinum rounded-md'></div>
-					<div className='flex flex-col justify-around py-3'>
-						<a
-							href='https://www.github.com/ariicodes'
-							target='_blank'
-							rel='noreferrer'
-						>
-							<img
-								className='w-6'
-								src={github}
-								alt='github logo'
-							/>
-						</a>
-						<a
-							href='/'
-							target='_blank'
-							rel='noreferrer'
-						>
-							<img
-								className='w-6'
-								src={link}
-								alt='link icon'
-							/>
-						</a>
-					</div>
-				</div>
-				<h2>project name</h2>
-			</div> */}
+			<div>
+				<h2 className='text-xl font-semibold'>{title}</h2>
+			</div>
 		</div>
-	)
-}
+	);
+};
 
-export default Project
+export default Project;
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,14 +1,27 @@
-import Nav from '../components/Nav'
-import Header from '../components/Header'
-import Footer from '../components/Footer'
-import Project from '../components/Project'
+import Nav from '../components/Nav';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import Project from '../components/Project';
+import dish from '../assets/dish.png'
+import holiday from '../assets/holiday.png'
+import onlineLibrary from '../assets/online-library.png'
+
+const projectsList = [
+	{ icon: dish, altText: 'lineart plate wit fork and spoon', repo: 'https://github.com/ariicodes/recipe-tracker-app', url: 'https://recipe-tracker-app.ariicodes.com/', title: 'Recipe Tracker App'},
+	{ icon: holiday, altText: 'lineart beach chair and umbrella in front of a calendar', repo: 'https://github.com/ariicodes/thinkfulbnb', url: 'https://thinkfulbnb.ariicodes.com', title: 'Thinkfulbnb'},
+	{ icon: onlineLibrary, altText: 'lineart books in front of a computer', repo: 'https://github.com/ariicodes/local-library-project', url: 'https://local-library.ariicodes.com', title: 'Library App'},
+]
 
 const Projects = () => {
 	return (
 		<div className='w-screen h-screen flex flex-col justify-between'>
 			<div className='flex flex-row-reverse relative'>
 				<Header heading='Projects' />
-				<Project />
+				<div className='flex flex-col font-Urbanist w-8/12 pt-6 pl-6 absolute right-1/3 gap-4 md:-left-0.5 lg:text-xl lg:w-4/12 lg:flex-row lg:gap-6'>
+					{projectsList.map((project, index) => (
+						<Project key={index} icon={project.icon} altText={project.altText} repo={project.repo} url={project.url} title={project.title} />
+					))}
+				</div>
 			</div>
 			<div>
 				<div className='flex flex-row justify-between items-end'>
@@ -23,7 +36,7 @@ const Projects = () => {
 				<Footer />
 			</div>
 		</div>
-	)
-}
+	);
+};
 
-export default Projects
+export default Projects;
